8a707e944f9618f37720f9243c7831ff9f07c4d7
Daksh Rathod <daksh.rathod@dataphion.com>
2020-08-11 17:02:33 +0530

    changes

--- config/server.js
+++ config/server.js
@@ -9,10 +9,12 @@ module.exports = ({ env }) => ({
   rmq: env("RMQ_HOST", "amqp://localhost"),
   rmq_queue: env("RMQ_QUEUE_NAME", "highlight_queue"),
   strapi_host: env("STRAPI_HOST", "http://localhost:1337"),
-  vision_api_host: env("API_HOST", ""),
+  vision_api_host: env("VISION_API_HOST", "http://localhost:9502/vision/api"),
   protractor_host: env("PROTRACTOR_HOST", ""),
   protractor_path: env("PROTRACTOR_PATH", "/usr/bin/protractor"),
   executor_path: env("EXECUTOR_PATH", "/usr/src/dataphion/ai_testing"),
+  img_path: env("IMG_PATH", "/usr/src/dataphion/ai_testing"),
+  err_img_path: env("ERR_IMG_PATH", "/usr/src/dataphion/ai_testing"),
   testdecider: "testdecider",
   decider: "decider",
 
@@ -21,7 +23,16 @@ module.exports = ({ env }) => ({
   admin_lastname: env("STRAPI_ADMIN_LASTNAME", "QA"),
   admin_email: env("STRAPI_ADMIN_EMAIL", "urvesh.dungrani@dataphion.com"),
   admin_password: env("STRAPI_ADMIN_PASSWORD", "Aitester@123"),
-  admin_login_url: env("STRAPI_ADMIN_LOGIN_URL", "http://localhost:1337/admin/login"),
-  admin_register_url: env("STRAPI_ADMIN_REGISTER_URL", "http://localhost:1337/admin/register-admin"),
-  role_public_url: env("STRAPI_ROLE_PUBLIC_URL", "http://localhost:1337/users-permissions/roles/2"),
+  admin_login_url: env(
+    "STRAPI_ADMIN_LOGIN_URL",
+    "http://localhost:1337/admin/login"
+  ),
+  admin_register_url: env(
+    "STRAPI_ADMIN_REGISTER_URL",
+    "http://localhost:1337/admin/register-admin"
+  ),
+  role_public_url: env(
+    "STRAPI_ROLE_PUBLIC_URL",
+    "http://localhost:1337/users-permissions/roles/2"
+  ),
 });
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 set -ea
 
-
-if [ ! -f "package.json" ]; then
-
-  DATABASE_CLIENT=${DATABASE_CLIENT:-sqlite}
-
-  EXTRA_ARGS=${EXTRA_ARGS}
-
-  echo "Using strapi $(strapi -v)"
-  echo "No project found at /srv/app. Creating a new strapi project"
-
-  DOCKER=true strapi new . \
-    --dbclient=$DATABASE_CLIENT \
-    --dbhost=$DATABASE_HOST \
-    --dbport=$DATABASE_PORT \
-    --dbname=$DATABASE_NAME \
-    --dbusername=$DATABASE_USERNAME \
-    --dbpassword=$DATABASE_PASSWORD \
-    --dbssl=$DATABASE_SSL \
-    $EXTRA_ARGS
-
-elif [ ! -d "node_modules" ] || [ ! "$(ls -qAL node_modules 2>/dev/null)" ]; then
+if [ ! -d "node_modules" ] || [ ! "$(ls -qAL node_modules 2>/dev/null)" ]; then
 
   echo "Node modules not installed. Installing..."
 
