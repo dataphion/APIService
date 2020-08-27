9de95f1ad98822cab1522b745edf60f3f2f492af
pawan dhami <pawan.dhami@dataphion.com>
2020-08-27 17:55:06 +0530

    kafka connection

--- api/dbregistration/services/ConnectionCheck.js
+++ api/dbregistration/services/ConnectionCheck.js
@@ -5,13 +5,15 @@ const asyncRedis = require("async-redis");
 const amqp = require("amqplib/callback_api");
 const mongo = require("mongodb").MongoClient;
 const mssql = require("mssql");
+var kafka = require("kafka-node"),
+  Producer = kafka.Producer;
 
 /**
  * `ConnectionCheck` service.
  */
 
 module.exports = {
-  check: async data => {
+  check: async (data) => {
     try {
       let status = "";
 
@@ -27,6 +29,8 @@ module.exports = {
         status = await checkMongoDBConnection(data);
       } else if (data.database_type === "mssql") {
         status = await checkMSSQLConnection(data);
+      } else if (data.database_type === "kafka") {
+        status = await checkKafkaConnection(data);
       }
 
       return { status: status };
@@ -35,14 +39,14 @@ module.exports = {
       console.error(error);
       return { status: "failed", error };
     }
-  }
+  },
 };
 
 /**
  * @param  {object} creds
  * @description check the mysql connectivity
  */
-const checkMysqlConnection = creds => {
+const checkMysqlConnection = (creds) => {
   return new Promise((resolve, reject) => {
     try {
       const connectionObject = {
@@ -50,7 +54,7 @@ const checkMysqlConnection = creds => {
         port: creds.port,
         user: creds.username,
         password: creds.password,
-        database: creds.database
+        database: creds.database,
       };
 
       mysql
@@ -58,7 +62,7 @@ const checkMysqlConnection = creds => {
         .then(() => {
           resolve("success");
         })
-        .catch(err => {
+        .catch((err) => {
           reject(err);
         });
     } catch (error) {
@@ -71,15 +75,46 @@ const checkMysqlConnection = creds => {
 
 /**
  * @param  {object} creds
+ * @description check the kafka connectivity
+ */
+const checkKafkaConnection = (creds) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const client = new kafka.KafkaClient({
+        kafkaHost: `${creds.ip}:${creds.port}`,
+      });
+      producer = new Producer(client);
+
+      // console.log(producer);
+
+      producer.on("ready", function () {
+        console.log("Connected");
+        resolve("success");
+      });
+
+      producer.on("error", function (err) {
+        console.error("Error occurred:", err);
+        reject(err);
+      });
+    } catch (error) {
+      console.error("Error in checkOracleConnection");
+      console.error(error);
+      reject(error);
+    }
+  });
+};
+
+/**
+ * @param  {object} creds
  * @description check the oracle connectivity
  */
-const checkOracleConnection = creds => {
+const checkOracleConnection = (creds) => {
   return new Promise((resolve, reject) => {
     try {
       const connectionObject = {
         user: creds.username,
         password: creds.password,
-        connectString: `${creds.ip}:${creds.port}/${creds.database}`
+        connectString: `${creds.ip}:${creds.port}/${creds.database}`,
       };
 
       oracledb
@@ -87,7 +122,7 @@ const checkOracleConnection = creds => {
         .then(() => {
           resolve("success");
         })
-        .catch(err => {
+        .catch((err) => {
           reject(err);
         });
     } catch (error) {
@@ -102,12 +137,12 @@ const checkOracleConnection = creds => {
  * @param  {object} creds
  * @description check the redis connectivity
  */
-const checkRedisConnection = creds => {
+const checkRedisConnection = (creds) => {
   return new Promise((resolve, reject) => {
     try {
       const client = asyncRedis.createClient(creds.port, creds.ip);
 
-      client.on("error", err => {
+      client.on("error", (err) => {
         client.quit(); // terminate the connection before sending the message back
         reject(err);
       });
@@ -127,12 +162,12 @@ const checkRedisConnection = creds => {
  * @param  {object} creds
  * @description check the rabbitmq connectivity
  */
-const checkRMQConnection = creds => {
+const checkRMQConnection = (creds) => {
   return new Promise((resolve, reject) => {
     try {
       amqp.connect(
         `amqp://${creds.username}:${creds.password}@${creds.ip}:${creds.port}`,
-        function(err) {
+        function (err) {
           if (err) {
             reject(err.toString());
           } else {
@@ -148,14 +183,16 @@ const checkRMQConnection = creds => {
   });
 };
 
-const checkMongoDBConnection = creds => {
+const checkMongoDBConnection = (creds) => {
   return new Promise((resolve, reject) => {
     try {
-      console.log(`mongodb://${creds.username}:${creds.password}@${creds.ip}:${creds.port}/${creds.database}`);
-      
+      console.log(
+        `mongodb://${creds.username}:${creds.password}@${creds.ip}:${creds.port}/${creds.database}`
+      );
+
       mongo.connect(
         `mongodb://${creds.username}:${creds.password}@${creds.ip}:${creds.port}/${creds.database}?authSource=admin`,
-        err => {
+        (err) => {
           if (err) {
             reject(err.toString());
           } else {
@@ -171,7 +208,7 @@ const checkMongoDBConnection = creds => {
   });
 };
 
-const checkMSSQLConnection = creds => {
+const checkMSSQLConnection = (creds) => {
   return new Promise((resolve, reject) => {
     try {
       const pool = new mssql.ConnectionPool({
@@ -179,10 +216,10 @@ const checkMSSQLConnection = creds => {
         port: Number(creds.port),
         user: creds.username,
         password: creds.password,
-        database: creds.database
+        database: creds.database,
       });
 
-      pool.connect(err => {
+      pool.connect((err) => {
         if (err) {
           reject(err.toString());
         } else {
