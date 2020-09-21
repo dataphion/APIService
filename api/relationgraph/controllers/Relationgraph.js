2058c19acbde2b9f646ef1306c59d5361e56287e
pawan dhami <pawan.dhami@dataphion.com>
2020-09-21 17:20:09 +0530

    relation graph fixed

--- api/relationgraph/controllers/Relationgraph.js
+++ api/relationgraph/controllers/Relationgraph.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
@@ -6,39 +6,38 @@
  */
 
 module.exports = {
-
-  creates: async ctx => {
+  creates: async (ctx) => {
     var bodyData = ctx.request.body;
-    let pg_conf = strapi.config.connections.default.settings
-    pg_conf['user'] = strapi.config.connections.default.settings.username
+    let pg_conf = strapi.config.connections.default.settings;
+    pg_conf["user"] = strapi.config.connections.default.settings.username;
 
-    if (!bodyData.hasOwnProperty('edges')) {
-      return {status: "fail"}
+    if (!bodyData.hasOwnProperty("edges")) {
+      return { status: "fail" };
     }
 
     if (bodyData.edges.length <= 0) {
       return {
-        status: "success"
-      }
+        status: "success",
+      };
     }
-    const {
-      Client
-    } = require("pg");
+    const { Client } = require("pg");
     const client = new Client(pg_conf);
     client.connect();
-	  console.log("data---", bodyData)
-    const query_start = `insert into relationgraphs(source,destination,node_name,node_request,data_key,source_label,destination_label, destination_method, source_method,application) values('${bodyData.edges[0].source}','${bodyData.edges[0].destination}','${bodyData.edges[0].node_name}','${bodyData.edges[0].node_request}','${bodyData.edges[0].data_key}','${bodyData.edges[0].source_label}','${bodyData.edges[0].destination_label}', '${bodyData.edges[0].destination_method}', '${bodyData.edges[0].source_method}','${bodyData.edges[0].application}')`
+    console.log("data---", bodyData);
+    const query_start = `insert into relationgraphs(source,destination,node_name,node_request,data_key,source_label,destination_label, destination_method, source_method,application) values('${bodyData.edges[0].source}','${bodyData.edges[0].destination}','${bodyData.edges[0].node_name}','${bodyData.edges[0].node_request}','${bodyData.edges[0].data_key}','${bodyData.edges[0].source_label}','${bodyData.edges[0].destination_label}', '${bodyData.edges[0].destination_method}', '${bodyData.edges[0].source_method}','${bodyData.edges[0].application}')`;
     let query_betw = ``;
     const query_end = ` on conflict(source,destination,node_name,node_request,data_key) do nothing;`;
     if (bodyData.edges.length > 1) {
       for (let index = 1; index < bodyData.edges.length; index++) {
         const edge = bodyData.edges[index];
-        console.log(edge)
-        query_betw = query_betw + `,('${edge.source}','${edge.destination}','${edge.node_name}','${edge.node_request}','${edge.data_key}','${edge.source_label}','${edge.destination_label}','${edge.destination_method}', '${edge.source_method}','${edge.application}')`
+        console.log(edge);
+        query_betw =
+          query_betw +
+          `,('${edge.source}','${edge.destination}','${edge.node_name}','${edge.node_request}','${edge.data_key}','${edge.source_label}','${edge.destination_label}','${edge.destination_method}', '${edge.source_method}','${edge.application}')`;
       }
     }
-    const query = query_start;
-    // console.log(query)
+    const query = query_start + query_betw + query_end;
+    console.log(query);
     // console.log(query_start)
     // console.log(query_end)
     // console.log(query_betw)
@@ -47,7 +46,7 @@ module.exports = {
     client.end();
 
     return {
-      status: "success"
-    }
-  }
+      status: "success",
+    };
+  },
 };
